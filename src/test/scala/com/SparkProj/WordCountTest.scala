1947bcc860a8284a6cc269810277cbe02a923ddf
sambhatt25 <38839329+sambhatt25@users.noreply.github.com>
2018-05-28 14:46:15 +0530

    Create WordCountTest.scala

---
+++ src/test/scala/com/SparkProj/WordCountTest.scala
@@ -0,0 +1,40 @@
+package com.SparkProj
+
+
+// Java
+import java.io.{FileWriter, File}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+// Scala
+import scala.io.Source
+
+// Google Guava
+import com.google.common.io.Files
+
+// Specs2
+//import org.specs2.mutable.Specification
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class WordCountTest extends FunSuite {
+
+  test("WordCount_test"){
+      val tempDir = Files.createTempDir()
+      val inputFile = new File(tempDir, "input").getAbsolutePath
+      val inWriter = new FileWriter(inputFile)
+      inWriter.write("hack hack hack and hack")
+      inWriter.close
+      val outputDir = new File(tempDir, "output").getAbsolutePath
+
+      WordCount.execute(
+        master = "local",
+        args   = List(inputFile, outputDir)
+      )
+
+      val outputFile = new File(outputDir, "part-00000")
+      val actual = Source.fromFile(outputFile).mkString
+      assert(actual=== "(hack,4)\n(and,1)\n")
+    }
+  
+}
